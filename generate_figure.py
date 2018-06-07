4b6469af81a797af50edec2ad47a1c6ec7719a98
stephaniealley <32311792+stephaniealley@users.noreply.github.com>
2018-06-07 02:02:39 -0400

    Update generate_figure.py

--- generate_figure.py
+++ generate_figure.py
@@ -1,59 +1,136 @@
 #!/usr/bin/env python
 #
-# Generate figure for generic spine data.
+# Automatically generate a figure for generic spine data displaying metric values for a specified contrast (t1, t2, dmri, mt, or gre-me).
 #
 # USAGE:
 # The script should be launched using SCT's python:
-#   ${SCT_DIR}/python/bin/python ${PATH_SPINEGENERIC}/generate_figure.py -i <image1> <image2>...
+#   ${SCT_DIR}/python/bin/python ${PATH_TO_SPINE_GENERIC}generate_figure.py -c contrast
 #
 # OUTPUT:
-# Figure
+# results_per_center.csv: metric results for each center
+# Figure displaying results across centers
 #
 # Authors: Stephanie Alley
-# License: https://github.com/neuropoly/gm_challenge/blob/master/LICENSE
+# License: https://github.com/neuropoly/sct_pipeline/spine_generic/blob/master/LICENSE
 
-import os, sys
-import argparse
-import numpy as np
-import nibabel as nib
+import os, argparse
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def get_parameters():
-    parser = argparse.ArgumentParser(description='Create a figure composed of axial slices of each of the files provided.')
-    parser.add_argument("-i", "--input",
-                        help="List the files to include in the figure, separated by a space.",
-                        nargs='+',
-                        required=True)
+    parser = argparse.ArgumentParser(description='Generate a figure to display metric values across centers.')
+    parser.add_argument("-c", "--contrast",
+                        help="Contrast for which figure should be generated.")
     args = parser.parse_args()
     return args
 
 def main():
-	list_of_files = args.input
+	# Data folder containing all centers
+    folder_dir = '/Volumes/projects/generic_spine_procotol/data'
+    # Folder name for each center
+    folder_list = ['20171128_glen', '20171207_ucl', '20171127_douglas', '20171221_poly', '20171209_oxford']
+    # Column labels in dataframe
+    centers = ['Ingenia-Glen', 'Achieva-UCL', 'Trio-Douglas', 'Skyra-Polytechnique', 'Prisma-Oxford']
+    # Row labels in dataframe
+    if contrast == 't1' or contrast == 't2':
+        vert_levels = ['C2', 'C3', 'C4', 'C5', 'C6', 'C7']
+    elif contrast == 'dmri' or contrast == 'mt':
+        vert_levels = ['C2', 'C3', 'C4', 'C5']
+    elif contrast == 'gre-me':
+        vert_levels = ['C3', 'C4']
+    # Define colors for each bar in graph
+    colors = ['dodgerblue', 'dodgerblue', 'limegreen', 'limegreen', 'limegreen']
+    # Output file containing metric values for all centers
+    file_output = "results_per_center.csv"
 
-	# Initiate the figure
-	fig = plt.figure(figsize=(6,2))
-	# Define position of first slice in the figure
-	p = 1
-	# For each file provided, add z slices to the figure
-	for file in list_of_files:
-		# Load NIfTI file
-		im = nib.load(file)
-		# Get image data as an array
-		im_array = im.get_data().T
-		# Get the number of z slices
-		slice_num = im_array.shape[0]
-		# Add each slice to the figure
-		for slice in range(slice_num):
-			# Add slice as a subplot
-			fig.add_subplot(len(list_of_files),slice_num,p)
-			plt.imshow(im_array[slice], cmap=plt.cm.gray, origin='lower')
-			plt.axis('off')
-			fig.subplots_adjust(hspace=0, wspace=0)
-			plt.savefig('im.png')
-			p += 1
+    # Initialize dataframe
+    results_per_center = pd.DataFrame(columns=centers, index=vert_levels)
+    
+    # Generate figure and results file for contrast
+    if contrast == 't1':
+        for folder in folder_list:
+            # Read in metric results for contrast
+            data = pd.read_excel(os.path.join(folder_dir, folder, contrast, 'csa/csa_mean.xls'), parse_cols = "G")
+            # Add results to dataframe
+            results_per_center[str(centers[folder_list.index(folder)])] = data['MEAN across slices'].values
+    
+        # Write results to file
+        results_per_center.to_csv(file_output)
 
-	plt.show()
+        # Generate figure for results
+        fig = results_per_center.plot(kind='bar', color=colors, figsize=(8, 8), legend=True, fontsize=15, align='center')
+        fig.set_xlabel("Vertebral level", fontsize=15, rotation='horizontal')
+        fig.set_ylabel("CSA ($mm^2$)", fontsize=15)
+        plt.title('T1w')
+        plt.savefig('t1.png')
+    elif contrast =='t2':
+        for folder in folder_list:
+            # Read in metric results for contrast
+            data = pd.read_excel(os.path.join(folder_dir, folder, contrast, 'csa/csa_mean.xls'), parse_cols = "G")
+            # Add results to dataframe
+            results_per_center[str(centers[folder_list.index(folder)])] = data['MEAN across slices'].values
+    
+        # Write results to file
+        results_per_center.to_csv(file_output)
+
+        # Generate figure for results
+        fig = results_per_center.plot(kind='bar', color=colors, figsize=(8, 8), legend=True, fontsize=15, align='center')
+        fig.set_xlabel("Vertebral level", fontsize=15, rotation='horizontal')
+        fig.set_ylabel("CSA ($mm^2$)", fontsize=15)
+        plt.title('T2w')
+        plt.savefig('t2.png')
+    elif contrast == 'dmri':
+        for folder in folder_list:
+            # Read in metric results for contrast
+            data = pd.read_excel(os.path.join(folder_dir, folder, contrast, 'fa.xls'), parse_cols = "I")
+            # Add results to dataframe
+            results_per_center[str(centers[folder_list.index(folder)])] = data['Metric value'].values
+    
+        # Write results to file
+        results_per_center.to_csv(file_output)
+
+        # Generate figure for results
+        fig = results_per_center.plot(kind='bar', color=colors, figsize=(8, 8), legend=True, fontsize=15, align='center')
+        fig.set_xlabel("Vertebral level", fontsize=15, rotation='horizontal')
+        fig.set_ylabel("FA", fontsize=15)
+        plt.title('DWI')
+        plt.savefig('dwi.png')
+    elif contrast == 'mt':
+        for folder in folder_list:
+            # Read in metric results for contrast
+            data = pd.read_excel(os.path.join(folder_dir, folder, contrast, 'mtr.xls'), parse_cols = "I")
+            # Add results to dataframe
+            results_per_center[str(centers[folder_list.index(folder)])] = data['Metric value'].values
+    
+        # Write results to file
+        results_per_center.to_csv(file_output)
+
+        # Generate figure for results
+        fig = results_per_center.plot(kind='bar', color=colors, figsize=(8, 8), legend=True, fontsize=15, align='center')
+        fig.set_xlabel("Vertebral level", fontsize=15, rotation='horizontal')
+        fig.set_ylabel("MTR (%)", fontsize=15)
+        plt.title('MTR')
+        plt.savefig('mt.png')
+    elif contrast == 'gre-me':
+        for folder in folder_list:
+            # Read in metric results for contrast
+            data = pd.read_excel(os.path.join(folder_dir, folder, contrast, 'csa/csa_mean.xls'), parse_cols = "G")
+            # Add results to dataframe
+            results_per_center[str(centers[folder_list.index(folder)])] = data['MEAN across slices'].values
+    
+        # Write results to file
+        results_per_center.to_csv(file_output)
+
+        # Generate figure for results
+        fig = results_per_center.plot(kind='bar', color=colors, figsize=(8, 8), legend=True, fontsize=15, align='center')
+        fig.set_xlabel("Vertebral level", fontsize=15, rotation='horizontal')
+        fig.set_ylabel("CSA ($mm^2$)", fontsize=15)
+        plt.title('GRE-ME')
+        plt.savefig('gre-me.png')
+
+    plt.show()
 
 if __name__ == "__main__":
     args = get_parameters()
+    contrast = args.contrast
     main()
