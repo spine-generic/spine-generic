f2518c6db3d6cc127173c965aa680cbd625dc7a4
Nicolas <34742864+Nicolas-Pinon@users.noreply.github.com>
2019-01-28 16:16:19 -0500

    Update generate_ground_truth.sh

--- generate_ground_truth.sh
+++ generate_ground_truth.sh
@@ -29,36 +29,35 @@ cd anat/
 
 # MTS
 # ==============================================================================
-file_t1w="${sub}_acq-T1w_MTS"
+file_t1w_mts="${sub}_acq-T1w_MTS"
 file_mton="${sub}_acq-MTon_MTS"
 file_mtoff="${sub}_acq-MToff_MTS"
+file_t2w="${sub}_acq-T2w"
+file_t2s="${sub}_T2star"
+file_t1w="${sub}_T1w"
 # Check if manual segmentation already exists
 if [ -e "${file_t1w}_seg_manual.nii.gz" ]; then
   file_seg="${file_t1w}_seg_manual"
 else
   # Segment spinal cord
-  sct_deepseg_sc -i ${file_t1w}.nii.gz -c t1 -qc ${PATH_QC}
-  file_seg="${file_t1w}_seg"
+  sct_deepseg_sc -i ${file_t1w_mts}.nii.gz -c t1 -qc ${PATH_QC}
+  file_seg="${file_t1w_mts}_seg"
 fi
 # Create mask
-sct_create_mask -i ${file_t1w}.nii.gz -p centerline,${file_seg}.nii.gz -size 35mm -o ${file_t1w}_mask.nii.gz
-# Register PD->T1w
+sct_create_mask -i ${file_t1w_mts}.nii.gz -p centerline,${file_seg}.nii.gz -size 35mm -o ${file_t1w_mts}_mask.nii.gz
+
+# Registrations to T1w MTS :
 # Tips: here we only use rigid transformation because both images have very similar sequence parameters. We don't want to use SyN/BSplineSyN to avoid introducing spurious deformations.
-sct_register_multimodal -i ${file_mtoff}.nii.gz -d ${file_t1w}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
-# Register MT->T1w
-sct_register_multimodal -i ${file_mton}.nii.gz -d ${file_t1w}_crop.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
-
+# MToff
+sct_register_multimodal -i ${file_mtoff}.nii.gz -d ${file_t1w_mts}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
+# MTon
+sct_register_multimodal -i ${file_mton}.nii.gz -d ${file_t1w_mts}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
+# T2w
+sct_register_multimodal -i ${file_t2w}.nii.gz -d ${file_t1w_mts}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
+# T2star
+sct_register_multimodal -i ${file_t2s}.nii.gz -d ${file_t1w_mts}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
 # T1w
-# ==============================================================================
-file="${sub}_T1w"
-# Register to GRE-T1w
+sct_register_multimodal -i ${file_t1w}.nii.gz -d ${file_t1w_mts}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline
 
-# T2
-# ==============================================================================
-file_T2="${sub}_T2w"
-# Register to GRE-T1w
 
-# t2s
-# ==============================================================================
-file_T2s="${sub}_T2star"
-# Register to GRE-T1w
+
