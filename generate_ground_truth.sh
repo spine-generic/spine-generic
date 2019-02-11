ed49337cbffea2a1ea5883466847694efe08c6f2
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-02-11 11:45:39 -0500

    generate_ground_truth.sh: Added TODO

--- generate_ground_truth.sh
+++ generate_ground_truth.sh
@@ -61,6 +61,9 @@ sct_register_multimodal -i ${file_t2s}.nii.gz -d ${file_t1w_mts}.nii.gz -m ${ofo
 # T1w
 sct_register_multimodal -i ${file_t1w}.nii.gz -d ${file_t1w_mts}.nii.gz -m ${ofolder_reg}/${file_t1w_mts}_mask.nii.gz -param step=1,type=im,algo=slicereg,metric=CC,poly=2 -x spline -ofolder ${ofolder_reg}
 
+#TODO:
+crop all volumes to remove the first 3 slices and the last 3 slices
+
 # Delete useless images
 #rm "${ofolder_reg}/${file_t1w_mts}_mask.nii.gz"
 #rm "${ofolder_reg}/${file_t1w_mts}_reg.nii.gz"
