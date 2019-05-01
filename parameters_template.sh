84d0cdd9dcfb28c2f38d91e693f867caa0cd0408
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-04-30 23:10:30 -0400

    parameters_template.sh: Added fields with new run_process

--- parameters_template.sh
+++ parameters_template.sh
@@ -12,11 +12,20 @@ export PATH_DATA="${PATH_PARENT}/spineGeneric_multiSubjects"
 # List of subjects to analyse. Comment this variable if you want to analyze all
 # sites in the PATH_DATA folder.
 #export SITES=(
-#	"ucl"
-#	"unf"
+#	"amu_spineGeneric"
 #)
 
-# Paths to where to process and save results and QC report.
+# Paths to where to save the new dataset.
 # Do not add "/" at the end. Path should be absolute (i.e. do not use "~")
-export PATH_PROCESSING="${PATH_PARENT}/spineGeneric_multiSubjects_results"
-export PATH_QC="${PATH_PARENT}/spineGeneric_multiSubjects_qc"
+export PATH_OUTPUT="${PATH_PARENT}/spineGeneric/results"
+export PATH_QC="${PATH_PARENT}/spineGeneric/qc"
+export PATH_LOG="${PATH_PARENT}/spineGeneric/log"
+
+# Location of manually-corrected segmentations
+export PATH_SEGMANUAL="${PATH_PARENT}/spineGeneric/seg_manual"
+
+# Number of jobs for parallel processing
+export JOBS=20
+
+# Number of jobs for ANTs routine. Set to 1 if ANTs functions crash when CPU saturates.
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
