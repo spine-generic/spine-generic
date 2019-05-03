eb06ce69d2891642a90754239ae6e2c1d9471286
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-05-03 07:35:25 -0400

    process_data.sh: Clarified output if error

--- processing/process_data.sh
+++ processing/process_data.sh
@@ -225,23 +225,23 @@ cd ..
 # Verify presence of output files and write log file if error
 # ------------------------------------------------------------------------------
 FILES_TO_CHECK=(
-  "$PATH_IN/anat/${SUBJECT}_T1w_RPI_r_seg.nii.gz"
-  "$PATH_IN/anat/${SUBJECT}_T1w_RPI_r_seg_labeled.nii.gz"
-  "$PATH_IN/anat/${SUBJECT}_T2w_RPI_r_seg.nii.gz"
-  "$PATH_IN/anat/${SUBJECT}_T1w_RPI_r_seg_labeled2T2w_RPI_r.nii.gz"
-  "$PATH_IN/anat/label_axT1w/template/PAM50_levels.nii.gz"
-  "$PATH_IN/anat/mtr.nii.gz"
-  "$PATH_IN/anat/mtsat.nii.gz"
-  "$PATH_IN/anat/t1map.nii.gz"
-  "$PATH_IN/anat/${SUBJECT}_T2star_rms_gmseg.nii.gz"
-  "$PATH_IN/anat/${SUBJECT}_T1w_seg_labeled2T2star.nii.gz"
-  "$PATH_IN/dwi/dti_FA.nii.gz"
-  "$PATH_IN/dwi/dti_MD.nii.gz"
-  "$PATH_IN/dwi/dti_RD.nii.gz"
-  "$PATH_IN/dwi/label/atlas/PAM50_atlas_00.nii.gz"
+  "anat/${SUBJECT}_T1w_RPI_r_seg.nii.gz"
+  "anat/${SUBJECT}_T1w_RPI_r_seg_labeled.nii.gz"
+  "anat/${SUBJECT}_T2w_RPI_r_seg.nii.gz"
+  "anat/${SUBJECT}_T1w_RPI_r_seg_labeled2T2w_RPI_r.nii.gz"
+  "anat/label_axT1w/template/PAM50_levels.nii.gz"
+  "anat/mtr.nii.gz"
+  "anat/mtsat.nii.gz"
+  "anat/t1map.nii.gz"
+  "anat/${SUBJECT}_T2star_rms_gmseg.nii.gz"
+  "anat/${SUBJECT}_T1w_seg_labeled2T2star.nii.gz"
+  "dwi/dti_FA.nii.gz"
+  "dwi/dti_MD.nii.gz"
+  "dwi/dti_RD.nii.gz"
+  "dwi/label/atlas/PAM50_atlas_00.nii.gz"
 )
 for file in ${FILES_TO_CHECK[@]}; do
   if [ ! -e $file ]; then
-    echo "${SITE}/${file} does not exist" >> $PATH_LOG/_error_check_output_files.log
+    echo "${SITE}/${SUBJECT}/${file} does not exist" >> $PATH_LOG/_error_check_output_files.log
   fi
 done
