0a12371d374f65b8f357893d16f861ac9ac71348
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-05-10 15:47:38 -0400

    _run_with_log.sh: Removed $SITE

--- processing/_run_with_log.sh
+++ processing/_run_with_log.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#
+# Wrapper to be able to output stdout and stderr in text file
 
 TASK=$1
-subject=$2
-site=$3
-PATH_OUTPUT=$4
-PATH_QC=$5
-PATH_LOG=$6
+SUBJECT=$2
+PATH_OUTPUT=$3
+PATH_QC=$4
+PATH_LOG=$5
 
-cd ${PATH_DATA}/${site}
-${TASK} $(basename $subject) $site $PATH_OUTPUT $PATH_QC $PATH_LOG 2>&1 | tee ${PATH_LOG}/${site}_${subject}.log ; test ${PIPESTATUS[0]} -eq 0
+cd ${PATH_DATA}
+${TASK} $SUBJECT $PATH_OUTPUT $PATH_QC $PATH_LOG 2>&1 | tee ${PATH_LOG}/${SUBJECT}.log ; test ${PIPESTATUS[0]} -eq 0
 if [ ! $? -eq 0 ]; then
-  mv ${PATH_LOG}/${site}_${subject}.log ${PATH_LOG}/err.${site}_${subject}.log
+  mv ${PATH_LOG}/${SUBJECT}.log ${PATH_LOG}/err.${SUBJECT}.log
 fi
