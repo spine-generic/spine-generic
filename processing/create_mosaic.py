50a6dcd57060d4ef5f0df43a6bbac3972f85f2dd
charleygros <charley.gros@gmail.com>
2019-05-13 22:00:31 +1000

    create_mosaic: tmp folder

--- processing/create_mosaic.py
+++ processing/create_mosaic.py
@@ -18,24 +18,44 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+from spinalcordtoolbox.image import Image
+import sct_utils as sct
+
+
+def main():
+    # create temporary folder with intermediate results
+    tmp_folder = sct.TempFolder(verbose=verbose)
+    tmp_folder_path = tmp_folder.get_path()
+    print(tmp_folder_path)
+    # copy files to the tmp folder
+    path_tmp_lst = []
+    for f in path_im_lst:
+        tmp_folder.copy_from(f)
+        path_tmp_lst.append(os.path.basename(f))
+    print(path_tmp_lst)
+    tmp_folder.chdir()
+
+    # detect centerline
+
 
 def get_parameters():
     parser = argparse.ArgumentParser(
         description='Create a mosaic of images from different 3D data (param: -p), \
                     with axial or sagittal view (param: -ax_sag).')
-    parser.add_argument('-p', '--path',
+    parser.add_argument('-p', '--path_lst',
                         required=True,
                         help='List of paths to 3D images, separeted by comma.')
     parser.add_argument('-ax_sag', '--ax_sag',
                         required=False,
                         default='ax',
+                        choices=['ax','sag'],
                         help='Define the view of the samples:\
-                        \n\t- ax: axial view\
-                        \n\t- sag: sagittal view')
+                        ax --> axial view ; \
+                        sag --> sagittal view')
     parser.add_argument('-n', '--n_slice',
                         required=False,
                         default=1,
-                        help='Number of displayed samples.')  
+                        help='Number of displayed samples. Note that if ax_sag=sag, then n is forced to 1.')
     args = parser.parse_args()
     return args
 
