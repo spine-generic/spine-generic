6ac98fb57bf3144be05c4d101ce62b0e522b357a
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-20 16:37:07 -0400

    check_output_files: Fixed input param

--- processing/check_output_files.sh
+++ processing/check_output_files.sh
@@ -10,10 +10,7 @@ trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
 
 # Retrieve input params
 SUBJECT=$1
-SITE=$2
-PATH_OUTPUT=$3
-PATH_QC=$4
-PATH_LOG=$5
+FILEPARAM=$2
 
 # Create BIDS architecture
 PATH_IN="`pwd`/${SUBJECT}"
