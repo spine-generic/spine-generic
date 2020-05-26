c64cb1288041b7e112802ff01824468a21c14a5c
valosekj <jan.valosek@upol.cz>
2020-05-26 15:41:04 +0200

    move subplots closer to each other

--- processing/generate_figure.py
+++ processing/generate_figure.py
@@ -625,7 +625,7 @@ def main():
         frame.set_edgecolor('black')
         ax.add_artist(legend)
         # Dynamic scaling of individual subplots based on data
-        offset = 8
+        offset = 2
         lim_min = min(min(x), min(y))
         lim_max = max(max(x), max(y))
         plt.xlim(lim_min - offset, lim_max + offset)
@@ -646,7 +646,7 @@ def main():
         # Place regression equation to upper-left corner
         plt.text(0.1, 0.9,
                  "y = {0:.4}x + {1:.4}\nR\u00b2 = {2:.4}".format(float(slope), float(intercept), float(r2_sc)),
-                 ha='left', va='center', transform = ax.transAxes, fontsize=TICKSIZE, color=red,
+                 ha='left', va='center', transform = ax.transAxes, fontsize=TICKSIZE, color='red',
                  bbox=dict(boxstyle='round', facecolor='white', alpha=1))   # box around equation
         # Plot linear fit
         axes = plt.gca()
@@ -657,6 +657,8 @@ def main():
         # Add title above middle subplot
         if index == 1:
             plt.title("CSA agreement between T1w and T2w data per vendors", fontsize=FONTSIZE, pad=20)
+    # Move subplots closer to each other
+    plt.subplots_adjust(wspace=-0.5)
     plt.tight_layout()
     fname_fig = 'fig_t1_t2_agreement_per_vendor.png'
     plt.savefig(fname_fig, dpi=200)
