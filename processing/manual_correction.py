e9c64a8d55a74f13afbd04212859ae088463aea9
valosekj <jan.valosek@upol.cz>
2020-07-05 09:58:24 +0200

    move script to BIDS standard

--- processing/manual_correction.py
+++ processing/manual_correction.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
 #
-# Script for manual correction of spinal cord and gray matter segmentation and vertebral labeling
+# Script for manual correction of spinal cord and gray matter segmentation and vertebral labeling.
 #
-# Run this script in results/data folder
+# Run this script in results/data folder or specify this folder by -ifolder flag.
+#
+# Manually corrected files are saved under derivatives/ folder (BIDS standard).
 #
 # USAGE:
-# Create a files.yml file that list the files to manually correct, for the spinal cord segmentation (FILES_SEG_SC),
-# gray matter segmentation (FILES_SEG_GM) and vertebral labeling (FILES_LABEL), as per the example below:
+# Create a files.yml file that lists all files which manual correction has to be done on.
+# For the spinal cord or gray matter segmentation list these files under FILES_SEG key,
+# for vertebral labeling list individual subjects under FILES_LABEL key, as per the example below:
 #
-#FILES_SEG_SC:
+#FILES_SEG:
 #- sub-amu01_T1w_RPI_r.nii.gz
 #- sub-amu01_T1w_RP_r.nii
 #- sub-amu01_T2w_RPI_r.nii.gz
 #- sub-cardiff02_dwi_crop_moco_dwi_mean.nii.gz
-#FILES_SEG_GM:
 #- sub-amu01_T2star_rms.nii.gz
 #FILES_LABEL:
 #- sub-amu01
 #- sub-amu02
 #
-# THEN RUN:
-#       PATH_TO_SPINEGENERIC/processing/manual_correction.py -i files.yml -o ~/seg_manual
+# THEN RUN (results/data folder):
+#       PATH_TO_SPINEGENERIC/processing/manual_correction.py -i files.yml
+# OR
+#       PATH_TO_SPINEGENERIC/processing/manual_correction.py -i files.yml -ifolder <BIDS_dataset>
 #
-# Authors: Julien Cohen-Adad, Jan Valosek
+# Authors: Jan Valosek, Julien Cohen-Adad
 
 import os
 import sys
+import shutil
 import re
 
 import argparse
@@ -34,64 +39,75 @@ import yaml
 class ManualCorrection():
 
     def __init__(self):
-        pass
+        self.folder_derivatives = 'derivatives'
 
     def main(self):
 
-        # Get parser args
+        # get parser args
         parser = self.get_parser()
         self.arguments = parser.parse_args()
 
-        # Check if input yml file exists
+        # check if input yml file exists
         if os.path.isfile(self.arguments.i):
             fname_yml = self.arguments.i
         else:
             sys.exit("ERROR: Input yml file {} does not exist or path is wrong.".format(self.arguments.i))
 
-        # Read input yml file as dict
+        # fetch input yml file as dict
         with open(fname_yml, 'r') as stream:
             try:
                 dict_yml = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
 
-        path_segmanual = self.arguments.o
+        # path to BIDS folder (optional arg, otherwise ./)
+        if self.arguments.ifolder is not None:
+            if os.path.isdir(self.arguments.ifolder):
+                path_bids = self.arguments.ifolder
+            else:
+                sys.exit("ERROR: BIDS folder does not exist or path is wrong.".format(self.arguments.ifolder))
 
-        self.spinal_cord_correction(dict_yml, path_segmanual)
-        self.gray_matter_correction(dict_yml, path_segmanual)
-        self.labels_correction(dict_yml, path_segmanual)
+        self.segmentation_correction(dict_yml, path_bids)
+        self.labels_correction(dict_yml, path_bids)
 
-    def spinal_cord_correction(self, dict_yml, path_segmanual):
+    def segmentation_correction(self, dict_yml, path_bids):
         """
-        Function for manual correction of spinal cord segmentation
+        Function for manual correction of spinal cord or gray matter segmentation
         """
-        # Loop across spinal cord segmentation files
-        for file_sc in dict_yml["FILES_SEG_SC"]:
+        # Loop across segmentation files
+        for file in dict_yml["FILES_SEG"]:
 
             # extract subject using first delimiter '_'
-            subject = file_sc.split("_", 1)[0]
+            subject = file.split('_', 1)[0]
 
-            # check if file is under dwi/ or anat/ folder and get fname_data
-            if "dwi" in file_sc:
-                fname_data = os.path.join(subject,"dwi",file_sc)
+            # check if file is under dwi/ or anat/ folder and get fname_data and create path_output
+            if 'dwi' in file:
+                fname_data = os.path.join(subject, 'dwi', file)
+                path_output = os.path.join(path_bids, self.folder_derivatives, subject, 'dwi')
             else:
-                fname_data = os.path.join(subject,"anat",file_sc)
+                fname_data = os.path.join(subject, 'anat', file)
+                path_output = os.path.join(path_bids, self.folder_derivatives, subject, 'anat')
 
-            # get fname_seg and fname_seg_dest
-            fname_seg = re.sub(r'.nii.gz','_seg.nii.gz',fname_data)
-            fname_seg_dest = os.path.join(path_segmanual,re.sub(r'.nii.gz','_seg-manual.nii.gz',fname_data))
+            # distinguish between gray matter and spinal cord segmentation
+            if 'T2star' in fname_data:
+                # get fname_seg
+                fname_seg = os.path.join(path_bids, re.sub(r'.nii.gz','_gmseg.nii.gz',fname_data))
+                # create fname_seg_dest in derivatives folder
+                fname_seg_dest = os.path.join(path_bids, self.folder_derivatives,
+                                              re.sub(r'.nii.gz','_gmseg-manual.nii.gz',fname_data))
+            else:
+                # get fname_seg
+                fname_seg = os.path.join(path_bids, re.sub(r'.nii.gz', '_seg.nii.gz', fname_data))
+                # create fname_seg_dest in derivatives folder
+                fname_seg_dest = os.path.join(path_bids, self.folder_derivatives,
+                                              re.sub(r'.nii.gz', '_seg-manual.nii.gz', fname_data))
 
+            print(path_output)
             print(fname_seg, fname_seg_dest)
+            # os.makedirs(path_output, exist_ok=True)
+            # shutil.copy(fname_seg, fname_seg_dest)
 
-    def gray_matter_correction(self, dict_yml, path_segmanual):
-        """
-        Function for manual correction of gray matter segmentation
-        """
-        # Loop across gray matter segmentation files
-        for file_gm in dict_yml["FILES_SEG_GM"]:
-            print(file_gm)
-
-    def labels_correction(self, dict_yml, path_segmanual):
+    def labels_correction(self, dict_yml, path_bids):
         """
         Function for manual correction of vertebral labeling
         """
@@ -104,25 +120,26 @@ class ManualCorrection():
         parser function
         """
         parser = argparse.ArgumentParser(
-            description="Manual correction of spinal cord and gray matter segmentation and vertebral labeling.",
+            description='Manual correction of spinal cord and gray matter segmentation and vertebral labeling. '
+                        'Manually corrected files are saved under derivatives/ folder (BIDS standard).',
             add_help=False,
-            prog=os.path.basename(__file__).strip(".py")
+            prog=os.path.basename(__file__).strip('.py')
         )
 
-        mandatory = parser.add_argument_group("\nMANDATORY ARGUMENTS")
+        mandatory = parser.add_argument_group('\nMANDATORY ARGUMENTS')
         mandatory.add_argument(
-            "-i",
+            '-i',
             required=True,
-            metavar="<input yml file>",
-            help="Filename of yml file containing segmentation and vertebral labeling for manual correction."
+            metavar='<input yml file>',
+            help='Filename of yml file containing segmentation and vertebral labeling for manual correction.'
         )
 
         optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
         optional.add_argument(
-            "-o",
-            metavar="<output folder>",
-            help="Path to output folder where manual segmentation and labels will be saved. Default = ../../seg_manual",
-            default="../../seg_manual"
+            '-ifolder',
+            metavar='<input folder>',
+            help='Path to input folder with BIDS dataset',
+            default='./'
         )
 
         optional.add_argument(
