8097b4d661c7657ebfe8348991b3b8136043b547
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-10 12:35:19 -0400

    Moved get_parser() to the top (more visible)

--- processing/move_file_derivatives.py
+++ processing/move_file_derivatives.py
@@ -12,6 +12,21 @@ import argparse
 import utils
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Copy files from <path-in>/sub-xxx/anat/ to <path-out>/derivatives/labels/sub-xxx/anat/. All "
+                    "copied files should share a common suffix as in sub-xxx_SUFFIX.nii.gz (e.g., "
+                    "T1w_RPI_r_seg_labeled). If the derivatives folder does not exist, it will be created",
+        formatter_class=utils.SmartFormatter,
+        prog=os.path.basename(__file__).strip('.py')
+        )
+    parser.add_argument("-p", "--path", dest="path", required=True, type=str,
+                        help="Path to results folder")
+    parser.add_argument("-s", "--suffix", dest="suffix", required=True, type=str,
+                        help="Suffix of the input file as in sub-xxx_suffix.nii.gz (e.g., _T2w)")
+    return parser
+
+
 def move_files(path, suffix):
     os.chdir(path)  # go to results folder
     list_folder = os.listdir("./")
@@ -26,21 +41,6 @@ def move_files(path, suffix):
             os.makedirs(path_out, exist_ok=True)
             shutil.copy(path_tmp, path_out)
     print("%i files moved" % (c))
-
-
-def get_parser():
-    parser = argparse.ArgumentParser(
-        description="Copy files from <path-in>/sub-xxx/anat/ to <path-out>/derivatives/labels/sub-xxx/anat/. All "
-                    "copied files should share a common suffix as in sub-xxx_SUFFIX.nii.gz (e.g., "
-                    "T1w_RPI_r_seg_labeled). If the derivatives folder does not exist, it will be created",
-        formatter_class=utils.SmartFormatter,
-        prog=os.path.basename(__file__).strip('.py')
-        )
-    parser.add_argument("-p", "--path", dest="path", required=True, type=str,
-                        help="Path to results folder")
-    parser.add_argument("-s", "--suffix", dest="suffix", required=True, type=str,
-                        help="Suffix of the input file as in sub-xxx_suffix.nii.gz (e.g., _T2w)")
-    return parser
 
 
 def main():
