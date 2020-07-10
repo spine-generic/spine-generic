f25dcd8c86f529cdf74865b7505001d0390f04c3
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-10 12:34:55 -0400

    Added description in usage

--- processing/move_file_derivatives.py
+++ processing/move_file_derivatives.py
@@ -9,6 +9,8 @@ import os
 import shutil
 import argparse
 
+import utils
+
 
 def move_files(path, suffix):
     os.chdir(path)  # go to results folder
@@ -23,11 +25,17 @@ def move_files(path, suffix):
             path_out = derivatives + path_tmp
             os.makedirs(path_out, exist_ok=True)
             shutil.copy(path_tmp, path_out)
-    print ("%i files moved" % (c))
+    print("%i files moved" % (c))
 
 
 def get_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Copy files from <path-in>/sub-xxx/anat/ to <path-out>/derivatives/labels/sub-xxx/anat/. All "
+                    "copied files should share a common suffix as in sub-xxx_SUFFIX.nii.gz (e.g., "
+                    "T1w_RPI_r_seg_labeled). If the derivatives folder does not exist, it will be created",
+        formatter_class=utils.SmartFormatter,
+        prog=os.path.basename(__file__).strip('.py')
+        )
     parser.add_argument("-p", "--path", dest="path", required=True, type=str,
                         help="Path to results folder")
     parser.add_argument("-s", "--suffix", dest="suffix", required=True, type=str,
