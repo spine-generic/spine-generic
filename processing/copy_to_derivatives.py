62f4ab2256ffeed73c27d2aabd076b409d1a0887
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-10 14:31:13 -0400

    Added -suffix-out option

--- processing/copy_to_derivatives.py
+++ processing/copy_to_derivatives.py
@@ -27,11 +27,13 @@ def get_parser():
                         help="Path to input BIDS dataset, which contains all the 'sub-' folders.")
     parser.add_argument('-path-out', required=True, type=str,
                         help="Path to output BIDS dataset, which contains all the 'sub-' folders.")
-    parser.add_argument('-suffix', dest="suffix", required=True, type=str,
+    parser.add_argument('-suffix', required=True, type=str,
                         help="Suffix of the input file, as in sub-*<suffix>.nii.gz. The program will search for all "
                              "files with .nii.gz extension.")
+    parser.add_argument('-suffix-out', required=False, type=str,
+                        help="Suffix to add to the output (copied) file: sub-*<suffix><suffix-out>.nii.gz.")
     parser.add_argument('-v', '--verbose', action='store_true',
-        help="Full verbose (for debugging)")
+                        help="Full verbose (for debugging)")
     return parser
 
 
@@ -47,7 +49,7 @@ def main():
     else:
         coloredlogs.install(fmt='%(message)s', level='INFO')
 
-    utils.copy_files_that_match_suffix(args.path_in, args.suffix, args.path_out, FOLDER_DERIVATIVES)
+    utils.copy_files_that_match_suffix(args.path_in, args.suffix, args.path_out, FOLDER_DERIVATIVES, args.suffix_out)
 
 
 if __name__ == '__main__':
--- processing/utils.py
+++ processing/utils.py
@@ -175,13 +175,15 @@ def check_software_installed(list_software=['fsleyes', 'sct']):
     return install_ok
 
 
-def copy_files_that_match_suffix(path_in, suffix, path_bids_out, folder_derivatives, extension='.nii.gz'):
+def copy_files_that_match_suffix(path_in, suffix, path_bids_out, folder_derivatives, suffix_out='',
+                                 extension='.nii.gz'):
     """
     Crawl in BIDS directory, and copy files that match suffix
     :param path_in: Path to input BIDS dataset, which contains all the 'sub-' folders.
     :param suffix:
     :param path_bids_out: Path to output BIDS dataset, which contains all the 'sub-' folders.
     :param folder_derivatives: name of derivatives folder where to put the data
+    :param suffix_out: Suffix to add to the output (copied) file: sub-*<suffix><suffix-out>.nii.gz
     :param extension:
     :return:
     """
@@ -192,7 +194,7 @@ def copy_files_that_match_suffix(path_in, suffix, path_bids_out, folder_derivati
         path_out = Path(path_bids_out, folder_derivatives, bids.get_subject(file), bids.get_contrast(file))
         os.makedirs(path_out, exist_ok=True)
         # copy
-        fname_out = path_out.joinpath(file)
+        fname_out = path_out.joinpath(add_suffix(file, suffix_out))
         shutil.copy(fname, fname_out)
         logging.info("{} \n-> {}\n".format(fname, fname_out))
     logging.info("Number of files copied: {}".format(len(fnames)))
