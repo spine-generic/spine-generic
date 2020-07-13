8439bd4dc9594ef193ff59e5edcfa5f1341dfaaa
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-13 15:19:46 -0400

    Added spinegeneric in path for import
    Added root folder in sys path
    
    tried something else...
    
    tried another thing
    
    test..
    
    test
    
    working

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
+print(sys.path)
 from spinegeneric import __version__
 
 
@@ -26,9 +29,6 @@ author = u'Julien Cohen-Adad'
 
 # The short X.Y version
 version = __version__
-
-# The full version, including alpha/beta/rc tags
-# release = u'1.0'
 
 
 # -- General configuration ---------------------------------------------------
