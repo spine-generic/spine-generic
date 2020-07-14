a91457aef9ef7085fcc3e759a44520205871eec9
Alexandru Foias <alexandrufoias@gmail.com>
2020-07-14 16:06:18 -0400

    change contrast list

--- spinegeneric/cli/params_checker.py
+++ spinegeneric/cli/params_checker.py
@@ -33,8 +33,8 @@ def main():
     # Initialize the layout
     layout = BIDSLayout(data_path)
 
-    Contrast = ['T1w','T2w']
-    query = layout.get(suffix=Contrast,extension='nii.gz')
+    Contrast_list = ['T1w','T2w']
+    query = layout.get(suffix=Contrast_list,extension='nii.gz')
 
     path_specs = resource_filename('spinegeneric', 'cli/specs.json')
 
@@ -42,29 +42,30 @@ def main():
         data = json.load(json_file)
 
     #Loop across the contrast images to check parameters
-    for item in query:
-        if 'Manufacturer' in item.get_metadata():
-            Manufacturer=item.get_metadata()['Manufacturer']
-            if Manufacturer in data.keys():
-                ManufacturersModelName = item.get_metadata()['ManufacturersModelName']
-                if ManufacturersModelName in data[Manufacturer].keys():
-                    if 'SoftwareVersions' in item.get_metadata():
-                        SoftwareVersions=item.get_metadata()['SoftwareVersions']
-                    RepetitionTime=item.get_metadata()['RepetitionTime']
-                    if Manufacturer != 'GE':
-                        if data[Manufacturer][ManufacturersModelName][str(Contrast)]["RepetitionTime"] != RepetitionTime:
-                            logging.warning(' Incorrect RepetitionTime: ' + item.filename + '; TR=' + str(RepetitionTime) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["RepetitionTime"]))
-                    EchoTime=item.get_metadata()['EchoTime']
-                    if Manufacturer != 'GE':
-                        if (EchoTime - data[Manufacturer][ManufacturersModelName][str(Contrast)]["EchoTime"]) > 0.0001:
-                            logging.warning(' Incorrect EchoTime: ' + item.filename + '; TE=' + str(EchoTime) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["EchoTime"]))
-                    FlipAngle=item.get_metadata()['FlipAngle']
-                    if data[Manufacturer][ManufacturersModelName][str(Contrast)]["FlipAngle"] != FlipAngle:
-                        logging.warning(' Incorrect FlipAngle: ' + item.filename + '; FA=' + str(FlipAngle) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["FlipAngle"]))
-                else:
-                    logging.warning('Missing: '+ ManufacturersModelName)
-        else:
-            logging.warning('Missing Manufacturer in json sidecar')
+    for Contrast in Contrast_list:
+        for item in query:
+            if 'Manufacturer' in item.get_metadata():
+                Manufacturer=item.get_metadata()['Manufacturer']
+                if Manufacturer in data.keys():
+                    ManufacturersModelName = item.get_metadata()['ManufacturersModelName']
+                    if ManufacturersModelName in data[Manufacturer].keys():
+                        if 'SoftwareVersions' in item.get_metadata():
+                            SoftwareVersions=item.get_metadata()['SoftwareVersions']
+                        RepetitionTime=item.get_metadata()['RepetitionTime']
+                        if Manufacturer != 'GE':
+                            if data[Manufacturer][ManufacturersModelName][str(Contrast)]["RepetitionTime"] != RepetitionTime:
+                                logging.warning(' Incorrect RepetitionTime: ' + item.filename + '; TR=' + str(RepetitionTime) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["RepetitionTime"]))
+                        EchoTime=item.get_metadata()['EchoTime']
+                        if Manufacturer != 'GE':
+                            if (EchoTime - data[Manufacturer][ManufacturersModelName][str(Contrast)]["EchoTime"]) > 0.0001:
+                                logging.warning(' Incorrect EchoTime: ' + item.filename + '; TE=' + str(EchoTime) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["EchoTime"]))
+                        FlipAngle=item.get_metadata()['FlipAngle']
+                        if data[Manufacturer][ManufacturersModelName][str(Contrast)]["FlipAngle"] != FlipAngle:
+                            logging.warning(' Incorrect FlipAngle: ' + item.filename + '; FA=' + str(FlipAngle) + ' instead of ' + str(data[Manufacturer][ManufacturersModelName][str(Contrast)]["FlipAngle"]))
+                    else:
+                        logging.warning('Missing: '+ ManufacturersModelName)
+            else:
+                logging.warning('Missing Manufacturer in json sidecar')
 
     #Print WARNING log
     if path_warning_log :
