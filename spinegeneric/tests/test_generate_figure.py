d67434c934d27427057658063691e88190e832a1
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-08-17 16:44:50 -0400

    Added test for generate_figure

--- spinegeneric/tests/test_generate_figure.py
+++ spinegeneric/tests/test_generate_figure.py
@@ -3,8 +3,26 @@
 # Test script for generate_figure
 
 import os
+import subprocess
+import importlib.resources
+
+import spinegeneric
 
 
-def test_generate_figure():
+def test_generate_figure_help():
     """Check if CLI script can execute without error"""
-    assert os.system('sg_generate_figure -h') == 0
+    result = subprocess.run(['sg_generate_figure', '-h'])
+    assert result.returncode == 0
+
+
+def test_generate_figure_dummy():
+    """Check if figures are generated using dummy csv results"""
+    with importlib.resources.path(spinegeneric.tests, 'results_dummy') as path_results:
+        result = subprocess.run(['sg_generate_figure', '-path-results', path_results])
+        assert result.returncode == 0
+        files = ['fig_csa_t1.png',
+                 'fig_csa_t2.png',
+                 'fig_t1_t2_agreement_per_vendor.png',
+                 'fig_t1_t2_agreement.png']
+        is_file_created = [os.path.isfile(path_results.joinpath(file)) for file in files]
+        assert all(is_file_created)
