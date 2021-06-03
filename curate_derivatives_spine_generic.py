926afc36f3e0aefbd0c8190cea7972743ceb7aef
sandrine <sandrine.bedard@polymtl.ca>
2021-06-03 10:31:24 -0400

    add reorient to RPI and resampling for csfseg

--- curate_derivatives_spine_generic.py
+++ curate_derivatives_spine_generic.py
@@ -75,6 +75,24 @@ def remove_suffix(fname, suffix):
     return os.path.join(stem.replace(suffix, '') + ext)
 
 
+def curate_csgseg(fname):
+    """
+    Reorient to RPI and resample _csg_seg-manual.nii.gz images.
+    :param fname: absolute or relative file to curate.
+    :return: reoriented and resampled image.
+    """
+    os.system('sct_image -i {} -setorient RPI -o {}'.format(fname, sg.utils.add_suffix(fname, '_RPI')))
+    os.system('sct_resample -i {} -mm 0.8x0.8x0.8 -o {}'.format(sg.utils.add_suffix(fname, '_RPI'), sg.utils.add_suffix(fname, '_RPI_r')))
+
+    try:
+        os.remove(fname)
+        os.remove(sg.utils.add_suffix(fname, '_RPI'))
+    except OSError as e:  ## if failed, report it back to the user ##
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+
+    shutil.move(sg.utils.add_suffix(fname, '_RPI_r'), fname)
+
+
 def main():
     parser = get_parser()
     args = parser.parse_args()
@@ -97,9 +115,10 @@ def main():
         subject = sg.bids.get_subject(file)
         contrast = get_contrast(file)
         if contrast=='dwi':
-            file_curated = subject + '_rec-average_dwi_seg-manual.nii.gz'
+            file_curated = subject + '_rec-average_dwi_seg-manual.nii.gz' # Rename 
         else:
-            file_curated = remove_suffix(file, '_RPI_r')
+            file_curated = remove_suffix(file, '_RPI_r') # Remove suffix on T1w and T2w images
+            file_curated = remove_suffix(file_curated, '_rms') # Remove suffix on T2star images
         # Append file to list.    
         manual_correction_list['FILES_SEG'].append(remove_suffix(file_curated, '_seg-manual'))
         fname = os.path.join(args.path_in, subject, contrast, file)
@@ -108,6 +127,10 @@ def main():
         os.makedirs(os.path.join(path_out_deriv, subject, contrast), exist_ok=True)
         shutil.copy(fname, fname_label)
         
+        # Reorient and resample csfseg
+        if 'csfseg-manual.nii.gz' in fname_label.split('_'):
+            curate_csgseg(fname_label)
+
         # create json sidecar with the name of the expert rater if it doesn't exist.
         fname_json = fname.rstrip('.nii').rstrip('.nii.gz') + '.json'
         if not os.path.isfile(fname_json):
@@ -115,6 +138,7 @@ def main():
         else:
             fname_json_curated = fname_label.rstrip('.nii').rstrip('.nii.gz') + '.json'
             shutil.copy(fname_json, fname_json_curated)
+    # Create a yml list with the name of the derivatives.
     with open(os.path.join(args.path_out, 'manual_seg.yml'), 'w') as file:
         yaml.dump(manual_correction_list, file)
 
