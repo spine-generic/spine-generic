b563ecc69997dcbfc521bf679aec7a9bb18e0e38
Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>
2025-05-20 12:00:36 -0400

    Use two handlers for logging
    Sending the logging output to two places (both the terminal
    and a file) is exactly the kind of thing that should be
    handled by the (very featureful) logging library.
    
    As a bonus, the log messages now appear in the terminal at
    the time they are generated, rather than all at the end.

--- src/spinegeneric/cli/params_checker.py
+++ src/spinegeneric/cli/params_checker.py
@@ -39,19 +39,29 @@ def main():
     args = parser.parse_args()
 
     # Initialize logging
-    path_warning_log = args.path_in / "WARNING.log"
-    path_warning_log.unlink(missing_ok=True)
-    logging.basicConfig(
-        filename=str(path_warning_log),
-        format="%(levelname)s:%(message)s",
-        level=logging.DEBUG,
-    )
+    logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {"default": {"format": "%(levelname)s: %(message)s"}},
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "formatter": "default",
+                "filename": str(args.path_in / "WARNING.log"),
+                "mode": "w",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    })
 
     # Initialize the BIDSLayout object directed at the input dataset.
     # From the BIDS documentation:
     #   "A BIDSLayout instance is a lightweight container for all files in the BIDS project directory."
     with importlib.resources.path(spinegeneric.config, "bids_specs.json") as path_sg_layout_config:
-        # TODO: This step takes quite a long time, but nothing gets logged during. Maybe we could provide some feedback?
         layout = BIDSLayout(
             str(args.path_in),
             # BIDSLayoutIndexer is a class that indexes files based on pattern-matching defined in the config.
@@ -83,16 +93,16 @@ def main():
     for item in query:
         # Check that the json sidecar has the correct keys and values
         if "Manufacturer" not in item.get_metadata():
-            logging.warning(f" {item.filename}: Missing 'Manufacturer' key in json sidecar; Cannot check parameters.")
+            logging.warning(f"{item.filename}: Missing 'Manufacturer' key in json sidecar; Cannot check parameters.")
             continue
         Manufacturer = item.get_metadata()["Manufacturer"]
         if Manufacturer not in sg_acq_protocol.keys():
-            logging.warning(f" {item.filename}: Manufacturer '{Manufacturer}' not in list "
+            logging.warning(f"{item.filename}: Manufacturer '{Manufacturer}' not in list "
                             f"of known manufacturers: {sg_acq_protocol.keys()}. Cannot check parameters.")
             continue
         ManufacturersModelName = item.get_metadata()["ManufacturersModelName"]
         if ManufacturersModelName not in sg_acq_protocol[Manufacturer].keys():
-            logging.warning(f" {item.filename}: Model '{ManufacturersModelName}' not present in list of known "
+            logging.warning(f"{item.filename}: Model '{ManufacturersModelName}' not present in list of known "
                             f"models for manufacturer '{Manufacturer}'. Cannot check parameters.")
             continue
 
@@ -124,7 +134,7 @@ def main():
             ExpectedRT = sg_acq_protocol[Manufacturer][ManufacturersModelName][str(Contrast)]["RepetitionTime"]
             # TODO: We only check `val > 0.1`, rather than `abs(val) > 0.1`. Is this a bug?
             if RepetitionTime - ExpectedRT > 0.1:
-                logging.warning(f" {item.filename}: Incorrect RepetitionTime: "
+                logging.warning(f"{item.filename}: Incorrect RepetitionTime: "
                                 f"TR={RepetitionTime} instead of {ExpectedRT} +/- 0.1.")
 
         # Validate echo time against spine-generic's acquisition protocol
@@ -133,7 +143,7 @@ def main():
             ExpectedTE = sg_acq_protocol[Manufacturer][ManufacturersModelName][str(Contrast)]["EchoTime"]
             # TODO: We only check `val > 0.1`, rather than `abs(val) > 0.1`. Is this a bug?
             if EchoTime - ExpectedTE > 0.1:
-                logging.warning(f" {item.filename}: Incorrect EchoTime: "
+                logging.warning(f"{item.filename}: Incorrect EchoTime: "
                                 f"TE={EchoTime} instead of {ExpectedTE} +/- 0.1.")
 
         # Validate flip angle against spine-generic's acquisition protocol
@@ -141,8 +151,5 @@ def main():
         if "FlipAngle" in keys_contrast:
             ExpectedFA = sg_acq_protocol[Manufacturer][ManufacturersModelName][str(Contrast)]["FlipAngle"]
             if FlipAngle != ExpectedFA:
-                logging.warning(f" {item.filename}: Incorrect FlipAngle: "
+                logging.warning(f"{item.filename}: Incorrect FlipAngle: "
                                 f"FA={FlipAngle} instead of {ExpectedFA}.")
-
-    # Print WARNING log
-    print(path_warning_log.read_text())
