9f45fce2a1948a83b55da5c62c120fa01e87f025
Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>
2025-05-23 12:00:31 -0400

    Factor out `warnings_found` into a function

--- src/spinegeneric/cli/params_checker.py
+++ src/spinegeneric/cli/params_checker.py
@@ -61,6 +61,11 @@ def main():
     # Keep track of whether any warnings are found
     warnings_found = False
 
+    def warn(*args, **kwargs):
+        nonlocal warnings_found
+        warnings_found = True
+        logging.warning(*args, **kwargs)
+
     # Initialize the BIDSLayout object directed at the input dataset.
     # From the BIDS documentation:
     #   "A BIDSLayout instance is a lightweight container for all files in the BIDS project directory."
@@ -78,20 +83,26 @@ def main():
         metadata = item.get_metadata()
         # Check that the json sidecar has the correct keys and values
         if "Manufacturer" not in metadata:
-            logging.warning(f"{item.filename}: Missing 'Manufacturer' key in json sidecar; Cannot check parameters.")
-            warnings_found = True
+            warn(
+                f"{item.filename}: Missing 'Manufacturer' key in json "
+                f"sidecar; Cannot check parameters."
+            )
             continue
         Manufacturer = metadata["Manufacturer"]
         if Manufacturer not in sg_acq_protocol.keys():
-            logging.warning(f"{item.filename}: Manufacturer '{Manufacturer}' not in list "
-                            f"of known manufacturers: {sg_acq_protocol.keys()}. Cannot check parameters.")
-            warnings_found = True
+            warn(
+                f"{item.filename}: Manufacturer '{Manufacturer}' not in list "
+                f"of known manufacturers: {sg_acq_protocol.keys()}. Cannot "
+                f"check parameters."
+            )
             continue
         ManufacturersModelName = metadata["ManufacturersModelName"]
         if ManufacturersModelName not in sg_acq_protocol[Manufacturer].keys():
-            logging.warning(f"{item.filename}: Model '{ManufacturersModelName}' not present in list of known "
-                            f"models for manufacturer '{Manufacturer}'. Cannot check parameters.")
-            warnings_found = True
+            warn(
+                f"{item.filename}: Model '{ManufacturersModelName}' not "
+                f"present in list of known models for manufacturer "
+                f"'{Manufacturer}'. Cannot check parameters."
+            )
             continue
 
         # Parse the filename's BIDS entities and suffix
@@ -102,12 +113,10 @@ def main():
             try:
                 key, value = part.split("-", maxsplit=1)
             except ValueError:
-                logging.warning(f"{item.filename}: Ignoring bad filename entity: '{part}'.")
-                warnings_found = True
+                warn(f"{item.filename}: Ignoring bad filename entity: '{part}'.")
                 continue
             if key in entities:
-                logging.warning(f"{item.filename}: Repeated entity in filename: '{key}'.")
-                warnings_found = True
+                warn(f"{item.filename}: Repeated entity in filename: '{key}'.")
             entities[key] = value
 
         # Get the contrast from the filename.
@@ -128,8 +137,7 @@ def main():
                 # Fall back to the old naming scheme
                 Contrast = f"{entities.get('acq', 'missing')}_MTS"
         if Contrast not in ["T1w", "T2w", "T2star", "MToff_MTS", "MTon_MTS", "T1w_MTS"]:
-            logging.warning(f"{item.filename}: Unrecognized contrast: '{Contrast}'")
-            warnings_found = True
+            warn(f"{item.filename}: Unrecognized contrast: '{Contrast}'")
             continue
 
         # Fetch the available parameters for the given manufacturer + model
@@ -146,34 +154,30 @@ def main():
                 # The protocol doesn't require this value.
                 continue
             if key not in metadata:
-                logging.warning(f"{item.filename}: Missing {key}.")
-                warnings_found = True
+                warn(f"{item.filename}: Missing {key}.")
                 continue
             if tolerance is None:
                 # We want an exact match of data type and value
                 if metadata[key] != expected[key]:
-                    logging.warning(
+                    warn(
                         f"{item.filename}: Incorrect {key}: {symbol}="
                         f"{metadata[key]!r} instead of {expected[key]!r}."
                     )
-                    warnings_found = True
             else:
                 # We want an approximate match of numerical values
                 try:
                     actual = float(metadata[key])
                 except ValueError:
-                    logging.warning(
+                    warn(
                         f"{item.filename}: Incorrect {key}: {symbol}="
                         f"{metadata[key]!r} is not a number."
                     )
-                    warnings_found = True
                     continue
                 if abs(actual - expected[key]) > tolerance:
-                    logging.warning(
+                    warn(
                         f"{item.filename}: Incorrect {key}: {symbol}={actual} "
                         f"instead of {expected[key]} +/- {tolerance}."
                     )
-                    warnings_found = True
 
     # Exit code for the script
     return 1 if warnings_found else 0
